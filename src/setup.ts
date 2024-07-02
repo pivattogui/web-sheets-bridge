f4eb5f9073f7f7cc071e6341ede483b30c8e001a
Pivatto <github@pivatto.dev>
2024-07-02 18:48:04 -0300

    fix

--- src/setup.ts
+++ src/setup.ts
@@ -1,19 +1,10 @@
-import AdmZip from "adm-zip"
 import { exec } from "child_process"
 import { promisify } from "util"
 
 const do_it = async () => {
     const execAwaitable = promisify(exec)
-
-    console.log("[🔧] Compilando Lambda do Cluster...")
+    console.log("Building...")
     await execAwaitable(`esbuild src/index.ts --bundle --external:aws-sdk --platform=node --target=es2020 --outfile=dist/index.js`)
-
-    //console.log("[🔧] Zipando Lambda do Cluster...")
-    //const zip = new AdmZip();
-    //zip.addLocalFile("./infra/lambda/dist/create-service.js", undefined, "index.js");
-
-
-
 }
 
 do_it()
